70a86fc67f73901eb1ea6fbc7010b194fc7ddc13
eastfisher <eastfisher72@gmail.com>
2021-09-05 10:45:32 +0800

    course-435: thumbor实现HTTP服务器, 协议定义和参数解析

---
+++ course-435/thumbor/src/main.rs
@@ -0,0 +1,57 @@
+use axum::{body::Bytes, extract::Path, handler::get, http::StatusCode, Router};
+use lru::LruCache;
+use percent_encoding::percent_decode_str;
+use proto::ImageSpec;
+use serde::Deserialize;
+use std::{convert::TryInto, net::SocketAddr, sync::Arc};
+use tokio::sync::Mutex;
+
+mod proto;
+
+use proto::*;
+
+// https://thumbor.readthedocs.io/en/latest/_images/logo-thumbor.png
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let _ = ImageSpec::new(Vec::new());
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/image/:spec/:url", get(generate));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url: {}\nspec: {:#?}", url, spec))
+}
+
+// httpie get "http://localhost:3000/image/CgoKCAjYBBCgBiADCgY6BAgUEBQKBDICCAM/https%3A%2F%2Fimages%2Epexels%2Ecom%2Fphotos%2F2470905%2Fpexels%2Dphoto%2D2470905%2Ejpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260"
---
+++ course-435/thumbor/src/proto/mod.rs
@@ -0,0 +1,76 @@
+use std::convert::TryFrom;
+
+use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
+use prost::Message;
+
+mod image;
+pub use image::*;
+
+use self::resize::ResizeType;
+
+impl ImageSpec {
+    pub fn new(specs: Vec<Spec>) -> Self {
+        Self { specs }
+    }
+}
+
+impl From<&ImageSpec> for String {
+    fn from(image_spec: &ImageSpec) -> Self {
+        let data = image_spec.encode_to_vec();
+        encode_config(data, URL_SAFE_NO_PAD)
+    }
+}
+
+impl TryFrom<&str> for ImageSpec {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let data = decode_config(value, URL_SAFE_NO_PAD)?;
+        let image_spec = ImageSpec::decode(&data[..])?;
+        Ok(image_spec)
+    }
+}
+
+impl Spec {
+    pub fn new_resize_seam_curve(width: u32, height: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width: width,
+                height: height,
+                rtype: resize::ResizeType::SeamCarve as i32,
+            })),
+        }
+    }
+
+    pub fn new_resize(width: u32, height: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width: width,
+                height: height,
+                rtype: resize::ResizeType::Normal as i32,
+            })),
+        }
+    }
+
+    pub fn new_watermark(x: u32, y: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Watermark(Watermark { x: x, y: y })),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{borrow::Borrow, convert::TryInto};
+
+    use super::*;
+
+    #[test]
+    fn encoded_spec_should_be_decoded() {
+        let spec1 = Spec::new_resize(600, 600);
+        let spec2 = Spec::new_resize_seam_curve(500, 500);
+        let image_spec = ImageSpec::new(vec![spec1, spec2]);
+        let s: String = image_spec.borrow().into();
+        assert_eq!(image_spec, s.as_str().try_into().unwrap());
+    }
+}
