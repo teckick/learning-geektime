96dde1d5cfcf32a0d77bbc6d9635611197bccb77
eastfisher <eastfisher72@gmail.com>
2021-09-05 11:19:08 +0800

    course-435: thumbor支持拉取图片

--- course-435/thumbor/src/main.rs
+++ course-435/thumbor/src/main.rs
@@ -1,10 +1,25 @@
-use axum::{body::Bytes, extract::Path, handler::get, http::StatusCode, Router};
+use anyhow::Result;
+use axum::{
+    body::Bytes,
+    extract::{Extension, Path},
+    handler::get,
+    http::{HeaderMap, HeaderValue, StatusCode},
+    AddExtensionLayer, Router,
+};
 use lru::LruCache;
 use percent_encoding::percent_decode_str;
 use proto::ImageSpec;
 use serde::Deserialize;
-use std::{convert::TryInto, net::SocketAddr, sync::Arc};
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    net::SocketAddr,
+    sync::Arc,
+};
 use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tracing::{info, instrument};
 
 mod proto;
 
@@ -24,11 +39,17 @@ type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let _ = ImageSpec::new(Vec::new());
+    // 这里为什么必须声明类型?
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(1024)));
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/image/:spec/:url", get(generate));
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -45,13 +66,49 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
     let url = percent_decode_str(&url).decode_utf8_lossy();
     let spec: ImageSpec = spec
         .as_str()
         .try_into()
         .map_err(|_| StatusCode::BAD_REQUEST)?;
-    Ok(format!("url: {}\nspec: {:#?}", url, spec))
+
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    // TODO: 处理图片
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("image/jpeg"));
+    Ok((headers, data.to_vec()))
+}
+
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+
+    Ok(data)
 }
 
 // httpie get "http://localhost:3000/image/CgoKCAjYBBCgBiADCgY6BAgUEBQKBDICCAM/https%3A%2F%2Fimages%2Epexels%2Ecom%2Fphotos%2F2470905%2Fpexels%2Dphoto%2D2470905%2Ejpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260"
